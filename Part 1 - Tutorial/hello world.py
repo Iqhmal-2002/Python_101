dc25207e625b0990965222e6bca7684b48c29ceb
Iqhmal-2002 <iqhmalworklife@gmail.com>
2026-06-22 08:49:03 +0800

    Testing push

---
+++ Part 1 - Tutorial/hello world.py
@@ -0,0 +1,40 @@
+# Comment
+print("hello world - python edition. \nFor real this time")
+
+# String
+first_name = "Shiki"
+last_name = "Takamiya"
+sephira = "Ein"
+
+print(f"Hello {first_name} {last_name}")
+print(f"{first_name}'s Sephira is {sephira}")
+
+#Int
+age = 15
+
+print(f"{first_name} is {age}")
+
+#Float
+float_num = 3.3
+
+print(f"This is a float {float_num}")
+
+is_online:bool = True
+
+# Type casting
+
+name = "Itsuka Shiki"
+age = 16
+has_Sephira = True
+level = 1.1
+
+level = int(level)
+print(f"Level after type casting is: {level}")
+print(str(float(int("88"))))
+print(str(int(float(int("88")))))
+
+bool1 = ""
+bool2 = None
+bool3 = "yyy"
+
+print(f"{bool(bool1)}, {bool(bool2)} {bool(bool3)}")
---
+++ Part 1 - Tutorial/input.py
@@ -0,0 +1,8 @@
+name = input("What is your name?\n")
+
+print(f"Your name is {name}")
+
+print("What is your last name?")
+last_name = input()
+
+print(f"{last_name}")
